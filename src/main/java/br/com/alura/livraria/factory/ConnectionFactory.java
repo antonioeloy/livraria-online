2e0e6386cc97f80d2053f8e0b5df286f5daadaf5
Antonio Eloy <antonioeloy14@gmail.com>
2021-09-28 19:01:06 -0300

    Atualizando ConnectionFactory

--- src/main/java/br/com/alura/livraria/factory/ConnectionFactory.java
+++ src/main/java/br/com/alura/livraria/factory/ConnectionFactory.java
@@ -3,23 +3,33 @@ package br.com.alura.livraria.factory;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
 
 public class ConnectionFactory {
 	
 	public Connection getConnection() {
 
-		try {
-			String url = "jdbc:mysql://localhost:3306/livraria";
-			String usuario = "root";
-			String senha = "root";
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection conexao = DriverManager.getConnection(url, usuario, senha);
-			return conexao;
-		} catch (SQLException | ClassNotFoundException e) {
-			System.out.println("Erro ao estabelecer conexão com o banco de dados.");
-			throw new RuntimeException(e);
+		Connection conexao = null;
+		Integer tentativasConexao = 0;
+		while (conexao == null && tentativasConexao <= 10) {
+			try {
+				String url = "jdbc:mysql://mysqldb:3306/livraria";
+				String usuario = "root";
+				String senha = "root";
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				conexao = DriverManager.getConnection(url, usuario, senha);
+				return conexao;
+			} catch (SQLException | ClassNotFoundException e) {
+				try {
+					TimeUnit.SECONDS.sleep(10);
+				} catch (InterruptedException ex) {
+					throw new RuntimeException(ex);
+				}
+				tentativasConexao++;
+				System.out.println("Erro ao estabelecer conexão com o banco de dados. Tentando novamente...");
+			}
 		}
-		
+		throw new RuntimeException("Não foi possível estabelecer conexão com o banco de dados.");
 	}
 
 }
